032b54e05f81d823afc074ae4d22eac4596d738e
valeponick <vale.ponick@gmail.com>
2026-05-19 23:28:23 +0700

    🔥chore: init CommitBestPractices project

--- CommitBestPractices/main.swift
+++ CommitBestPractices/main.swift
@@ -7,5 +7,31 @@
 
 import Foundation
 
-print("Hello, World!")
+// MARK: - 1️⃣ Data
+struct Commit {
+    let name: String
+    let description: String
+    let example: [String]
+    let emoji: String
+    let category: [String]
+    let whenToUse: String
+}
 
+enum Difficulty {
+    case easy
+    case medium
+    case hard
+}
+
+// MARK - 2️⃣ Данные (массив с примерами)
+
+var commits: [Commit] = [
+    Commit(
+        name: "feat",
+        description: "new functionality",
+        example: ["feat: add ToDo command", "✨ feat: add Commit struct and first data sample"],
+        emoji: "✨",
+        category: ["basic", "feature"],
+        whenToUse: "when add new command or opportunity"
+        )
+    ]
