45a65cf2b10d53c4e55ba472359b39b75034b50e
valeponick <vale.ponick@gmail.com>
2026-05-19 23:45:39 +0700

    📦 feat: add chore and fix commit types to array

--- CommitBestPractices/main.swift
+++ CommitBestPractices/main.swift
@@ -29,9 +29,25 @@ var commits: [Commit] = [
     Commit(
         name: "feat",
         description: "new functionality",
-        example: ["feat: add ToDo command", "✨ feat: add Commit struct and first data sample"],
+        example: ["feat: add chore and fix commit types to array", "feat: add Commit struct and first data sample"],
         emoji: "✨",
         category: ["basic", "feature"],
         whenToUse: "when add new command or opportunity"
+        ),
+    Commit(
+        name: "chore",
+        description: "maintenance tasks",
+        example: ["chore: init CommitBestPractices project", "chore: update dependencies"],
+        emoji: "🎉",
+        category: ["basic", "maintenance"],
+        whenToUse: "when creating project, setup configs, or updating tools"
+        ),
+    Commit(
+        name: "fix",
+        description: "bug fix",
+        example: ["fix: division by zero error"],
+        emoji: "🐛",
+        category: ["basic", "bug"],
+        whenToUse: "when fixing a bug in working code"
         )
     ]
